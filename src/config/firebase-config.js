a888790c100a09099a89ac84d23363b3f4ef0e0c
Sam Samiee <samsamiee00@gmail.com>
2025-07-29 18:57:39 +0200

    Lightbox added, new firebase structure

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyCUj250LL1QB4bz6ekVRkxW7Ge5YPEMkdk",
@@ -20,4 +21,4 @@ const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
-
+export const storage = getStorage(app);
